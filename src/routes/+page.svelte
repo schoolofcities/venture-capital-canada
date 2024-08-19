4e628a5021a7a0615713386782948412a2371962
Jeff Allen <jeffallen89@gmail.com>
2024-08-19 13:19:09 -0400

    top img

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,13 +37,14 @@
 		</div>
 	</div> -->
 
-	<br>
-	<br>
-	<br>
-	<br>
+
+	<div class="top-image">
+		<img src="./top.png"/>
+	</div>
 
 	
 	<div class="text">
+
 		<div class="title">
 			<h1>Canada’s High-Tech Startup Cities</h1>
 			<h3>The geography of venture capital investment across metro areas</h3>
@@ -54,7 +55,7 @@
 		</div>
 		<p>
 			<br>
-			<span id="drop-cap">V</span>enture capital is the financial fuel that powers innovation and economic growth. This unique form of equity financing — which exchanges capital for shares in the enterprises they provide — has fueled the rise of revolutionary companies like Intel in semiconductors, Apple in personal computing, Genentech in biotech, Google in search, Twitter/X in social media, Chat GPT in artificial intelligence, and many more. 
+			<span id="drop-cap">V</span>enture capital is the financial fuel that powers innovation and economic growth. This unique form of equity financing — which exchanges capital for shares in the enterprises they provide — has fueled the rise of revolutionary companies like Intel in semiconductors, Apple in personal computing, Genentech in biotech, Google in search, Twitter/X in social media, ChatGPT in artificial intelligence, and many more. 
 		</p>
 		<p>
 			This report examines trends in the geography of venture capital across Canada for the decade spanning the 2010s and 2020s. Our data on venture capital comes from <a href="https://pitchbook.com/" target="_blank">PitchBook</a>, a leading research firm that tracks venture capital investment. We group the data in multi-year periods, 2009-11 and 2019-21, to limit annual fluctuations that can occur because of outsized investments in individual firms and/or metros in any given year.
